94e2e66205bf4df392737fafc6fe94db19191856
Joseph Weidinger <jsphweid@gmail.com>
2018-01-29 11:51:23 -0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+bucket_name=$(<~/.aws/main_bucket_name)
+cloudfront_id=$(<~/.aws/main_cloudfront_id)
+
+aws s3 sync ./dist s3://${bucket_name}/feedCalc --delete 
+aws cloudfront create-invalidation --distribution-id ${cloudfront_id} --paths "/*"
